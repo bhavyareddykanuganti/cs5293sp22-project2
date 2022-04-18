c880058ff797f0da4f6b9a5a5c865bd04ffb4803
bhavyareddykanuganti <98802675+bhavyareddykanuganti@users.noreply.github.com>
2022-04-18 16:04:37 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
 	name='project2',
 	version='1.0',
-	author='You Name',
-	authour_email='your ou email',
+	author='Bhavya Reddy Kanuganti',
+	authour_email='bhavya.reddy.kanuganti-1@ou.edu',
 	packages=find_packages(exclude=('tests', 'docs')),
 	setup_requires=['pytest-runner'],
 	tests_require=['pytest']
